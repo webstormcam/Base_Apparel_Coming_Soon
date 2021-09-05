a53584e9fc5f571849aa9c7189d6b569a8aafcd1
Cameron Chandler <webstormcam@gmail.com>
2021-09-05 09:47:41 -0400

    I made the errors appear correctly
    Now I just gotta make it work

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
-function myFunc(){
-    var a = document.getElementById('')
+function validateForm(){
+    let x=document.getElementById('email').value;
+    let y = document.getElementById('reddot')
+    let z = document.getElementById('redwords')
+    if (x ===''){
+        y.style.display ='block'
+        z.style.display ='block'
+    
+    } else if (x.includes('@')==false){
+        y.style.display ='block'
+        z.style.display ='block'
+    
+    } else if ( x.includes('@')===true){
+       
+        document.getElementById("myForm").submit();
+     }
 }
+
