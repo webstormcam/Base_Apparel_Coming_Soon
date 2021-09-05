dffdaef0263183b44f6578e5a04933a89483c117
Cameron Chandler <webstormcam@gmail.com>
2021-09-05 10:04:40 -0400

    Put in the proper JS

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 function validateForm(){
-    let x=document.getElementById('email').value;
+    let x= document.getElementById('email').value;
     let y = document.getElementById('reddot')
     let z = document.getElementById('redwords')
     if (x ===''){
         y.style.display ='block'
         z.style.display ='block'
     
-    } else if (x.includes('@')==false){
+    } else if (x.includes('@') === false){
         y.style.display ='block'
         z.style.display ='block'
     
-    } else if ( x.includes('@')===true){
+    } else if(x.includes('@') === true && x.length<4){
+        y.style.display ='block'
+        z.style.display ='block'
+        
+    }else if ( x.includes('@') === true){
        
         document.getElementById("myForm").submit();
      }
